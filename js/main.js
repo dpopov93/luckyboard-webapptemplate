8e23e5dbb9a6fd90295881f9c9ac552a6cc2670b
Denis Popov <d.popov93@mail.ru>
2021-03-02 15:23:46 +0500

    Added functions for Popup messages

--- js/main.js
+++ js/main.js
@@ -1,9 +1,43 @@
 jQuery(document).ready(function($) {
+    
+    /************************************/
+    /* Functions for Popup info message */
+    
+    /* Timer for Message */
+    var showMessageTimer;
+    
+    /* Shows message width %textMessage% */
+    /* When timer is out message is close by closeMessage() */
+    function showMessage(textMessage) {
+        $(".message__text").text(textMessage);
+        $(".message").css("transform", "translateY(6rem)");
+        $(".app").css("transform", "translateY(25px)");
+        showMessageTimer = window.setTimeout(function(){closeMessage();}, 2500);
+    }
+    
+    /* Close message and stop timer */
+    function closeMessage() {
+        window.clearTimeout(showMessageTimer);
+        $(".message").css("transform", "translateY(-6rem)");
+        $(".app").css("transform", "translateY(-25px)");
+    }
+    
+    /* END: Functions for Popup info message */
+    /*****************************************/
+    
+    /**********************/
+    /* Main program logic */
+    
+    /* Message:on button close clicked */
     $(".message__btn-close").click(function(){
-        $(".message").css("transform", "translateY(-120px)");
+        closeMessage();
     });
 
+    /* On App component clicked */
     $(".app").click(function(){
-        $(".message").css("transform", "translateY(120px)");
+        showMessage("Hello");
     });
+    
+    /* END: Main program logic */
+    /***************************/
 });
